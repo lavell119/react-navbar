a886479dd30488de6313facb24ab367029064052
Lee <leewhiz119@gmail.com>
2022-08-17 11:38:21 -0400

    imported navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Navbar from './Navbar'
 
 function App() {
-  return <h1>Navbar</h1>
+  return <Navbar />
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,7 @@ export default function Navbar() {
         <ul>
             <li>
                 <a href="/pricing">Pricing</a>
+                <a href="/about">About</a>            
             </li>
         </ul>
     </nav>
