bd0abc4fa17fdb66648d76340b6580d5ad169378
Lee <leewhiz119@gmail.com>
2022-08-18 12:45:57 -0400

    imported page components and rendered

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Pricing from './pages/Pricing';
 
 function App() {
-  return <Navbar />
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home
+      break
+    case "/pricing":
+      Component = Pricing 
+      break
+    case "/about":
+      Component = About
+      break
+  }
+  return (
+    <>
+  <Navbar />
+  <Component />
+    </>
+)
 }
 
 export default App;
